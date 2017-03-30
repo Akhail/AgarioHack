7a845dbbff91a4283997a0aa3dfedbfc98d60c2a
Michel Betancourt <MichelBetancourt23@gmail.com>
2017-03-30 19:15:55 -0400

    Added PORT env

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express'),
     app = express();
     server = require('http').createServer(app);
-server.listen(3000);
+server.listen(process.env.POST || 3000);
 var io = require('socket.io').listen(server);
 
 app.use(express.static('public'));
