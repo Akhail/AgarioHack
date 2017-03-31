3ea6a1745b37f5bd07a2f4bfcbb8fffeb392d0f5
Michel Betancourt <MichelBetancourt23@gmail.com>
2017-03-31 14:52:28 -0400

    Try inline html client.js

--- public/client.js
+++ public/client.js
@@ -159,7 +159,7 @@
         mainpanel.css({'position': 'relative'});
 
         var main = $('<div>', {'class': 'optionScript'});
-        main.load(window.pserver + 'menu');        
+        main.append($('<h2>Menu Script</h2><div><h3>Codigo Amigo</h3><input type="text" id="pserver"></div><h4>Script Desarrollado por: Michel Betancourt(Akhail)</h4>'));        
         mainpanel.append(main);
     }
 
--- server.js
+++ server.js
@@ -4,20 +4,13 @@ var express = require('express'),
 server.listen(process.env.PORT || 3000);
 var io = require('socket.io').listen(server);
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "agariohere.com");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/public/client.js')
 });
 app.get('/style', function(req, res) {
     res.sendFile(__dirname + '/public/style.css');
 });
-app.get('/menu', function(req, res) {
-    res.sendFile(__dirname + '/public/menu.html');
-}) 
+
 
 var objects;
 
